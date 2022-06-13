93579a4da5ebbe4740cd7bf9f04c3e3d67a5b0d6
rosyrosie <kshunn@snu.ac.kr>
2022-06-13 23:23:16 +0900

    feat: show brand sync prop at Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { CORPLIST_URL } from "@api";
 import useAxios from "@useAxios";
-import { Button, Card, Divider, List, Rate, Segmented } from "antd";
+import { Button, Card, Divider, List, message, Rate, Segmented, Tag } from "antd";
 import { SegmentedValue } from "antd/lib/segmented";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -35,38 +35,54 @@ const Home = () => {
     navigate('/');
   }
 
+  useEffect(() => {
+    if(error) message.warning('잠시 후 다시 시도해주세요', 1.5);
+  }, [error]);
+
   return (
     <>
       <div className="brand_box">
         <Divider orientation="left">브랜드 목록</Divider>
         <div className="brand_list">
-          {corpList && <List 
-            grid={{
-              gutter: 16,
-              xs: 1,
-              sm: 2,
-              md: 2,
-              lg: 3,
-              xl: 3,
-              xxl: 4
-            }}
-            dataSource={corpList.corpList}
-            renderItem={(corp: any[]) => (
-              <List.Item onClick={() => setCorp(corp[0], corp[1])}>
-                <Card 
-                  hoverable
-                  bodyStyle={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    alignItems: 'center'
-                  }}
-                >
-                  {corp[1]}
-                  <Rate count={1} />
-                </Card>
-              </List.Item>
-            )}
-          />}
+          {corpList && 
+            <List 
+              grid={{
+                gutter: 16,
+                xs: 1,
+                sm: 2,
+                md: 2,
+                lg: 3,
+                xl: 3,
+                xxl: 4
+              }}
+              dataSource={corpList.corpList}
+              renderItem={(corp: any[]) => (
+                <List.Item onClick={() => setCorp(corp[0], corp[1])}>
+                  <Card 
+                    hoverable
+                    bodyStyle={{
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      alignItems: 'center'
+                    }}
+                  >
+                    <span>
+                      {corp[1]}
+                      <Tag
+                        color={corp[2] === 'linked' ? 'blue' : 'red'}
+                        style={{
+                          marginLeft: '6px'
+                        }}
+                      >
+                        {corp[2] === 'linked' ? '연동' : '미연동'}
+                      </Tag>
+                    </span>
+                    <Rate count={1} />
+                  </Card>
+                </List.Item>
+              )}
+            />
+          }
         </div>
         <div className="sortby">
           <Segmented options={['등록일순', '가나다순']} value={sortType} onChange={setSortType} />
