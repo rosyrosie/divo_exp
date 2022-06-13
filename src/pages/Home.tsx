d955a41fa16b901cc6113074f6649e3d093b558d
rosyrosie <kshunn@snu.ac.kr>
2022-06-13 20:59:04 +0900

    feat: add sorting to Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,17 @@
 import { CORPLIST_URL } from "@api";
 import useAxios from "@useAxios";
-import { Button, Card, Divider, List, Spin } from "antd";
-import { useEffect } from "react";
+import { Button, Card, Divider, List, Segmented, Spin } from "antd";
+import { SegmentedValue } from "antd/lib/segmented";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const navigate = useNavigate();
+  const [ sortType, setSortType ] = useState<SegmentedValue>('등록일순');
+  const sortId: Record<SegmentedValue, string> = {
+    '등록일순': 'id',
+    '가나다순': 'abc'
+  };
   let token = localStorage.getItem('token');
 
   useEffect(() => {
@@ -13,10 +19,10 @@ const Home = () => {
   }, [token]);
 
   const [ corpList, loading, error ] = useAxios(
-    CORPLIST_URL,
+    CORPLIST_URL + '?sortby=' + sortId[sortType],
     null,
     'GET',
-    []
+    [sortType]
   );
 
   const setCorp = (id: string, name: string) => {
@@ -32,31 +38,29 @@ const Home = () => {
   return (
     <>
       <div className="brand_box">
-        {
-          loading ? <Spin /> :
-          <>
-            <Divider orientation="left">브랜드 목록</Divider>
-            <div className="brand_list">
-              <List 
-                grid={{
-                  gutter: 16,
-                  xs: 1,
-                  sm: 2,
-                  md: 2,
-                  lg: 3,
-                  xl: 3,
-                  xxl: 4
-                }}
-                dataSource={corpList.corpList}
-                renderItem={(corp: any[]) => (
-                  <List.Item onClick={() => setCorp(corp[0], corp[1])}>
-                    <Card hoverable>{corp[1]}</Card>
-                  </List.Item>
-                )}
-              />
-            </div>
-          </>
-        }
+        <Divider orientation="left">브랜드 목록</Divider>
+        <div className="brand_list">
+          {corpList && <List 
+            grid={{
+              gutter: 16,
+              xs: 1,
+              sm: 2,
+              md: 2,
+              lg: 3,
+              xl: 3,
+              xxl: 4
+            }}
+            dataSource={corpList.corpList}
+            renderItem={(corp: any[]) => (
+              <List.Item onClick={() => setCorp(corp[0], corp[1])}>
+                <Card hoverable>{corp[1]}</Card>
+              </List.Item>
+            )}
+          />}
+        </div>
+        <div className="sortby">
+          <Segmented options={['등록일순', '가나다순']} value={sortType} onChange={setSortType} />
+        </div>
         <div className="logout">
           <Button onClick={handleLogout}>로그아웃</Button>
         </div>
